f4b5f28c517307656faa0694b8113d5eacd5eed4
Florian Fontan <dev@florian-fontan.fr>
2018-11-20 19:33:27 +0100

    Add to_string

--- info.hpp
+++ info.hpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/ini_parser.hpp>
@@ -20,9 +21,12 @@ struct Info
     std::string debug_string;
     std::chrono::high_resolution_clock::time_point t1;
 
-    void set_verbose()   { verbose_  = true; }
-    void set_debug()     { dbg_      = true; }
-    void set_debuglive() { dbg_live_ = true; }
+    void set_verbose()         { verbose_  = true; }
+    void set_verbose(bool b)   { verbose_  = b; }
+    void set_debug()           { dbg_      = true; }
+    void set_debug(bool b)     { dbg_      = b; }
+    void set_debuglive()       { dbg_live_ = true; }
+    void set_debuglive(bool b) { dbg_live_ = b; }
 
     bool verbose() { return verbose_; }
     bool debug() { return (dbg_ || dbg_live_); }
@@ -49,6 +53,15 @@ struct Info
             boost::property_tree::write_ini(file, pt);
     }
 
+    void write_dbg(std::string file)
+    {
+        if (file != "") {
+            std::ofstream f(file);
+            f << debug_string;
+            f.close();
+        }
+    }
+
     double elapsed_time() const
     {
         std::chrono::high_resolution_clock::time_point t2
@@ -56,6 +69,14 @@ struct Info
         std::chrono::duration<double> time_span
             = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
         return time_span.count();
+    }
+
+    template <class T>
+    static std::string to_string(T t)
+    {
+        std::ostringstream s;
+        s << t;
+        return s.str();
     }
 };
 
