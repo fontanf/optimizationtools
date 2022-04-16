d0cceac58c50da5850aa3447d1b545b36debe54c
Florian Fontan <dev@florian-fontan.fr>
2022-04-16 06:02:46 +0200

    Add intersection functions

--- optimizationtools/utils/utils.hpp
+++ optimizationtools/utils/utils.hpp
@@ -14,7 +14,18 @@ inline std::vector<std::string> split(std::string line, char c = ',');
 
 inline void hash_combine(std::size_t& seed, const size_t v);
 
-/******************************************************************************/
+template <typename T>
+inline bool on_segment(T x1, T y1, T x2, T y2, T x3, T y3);
+
+template <typename T>
+int orientation(T x1, T y1, T x2, T y2, T x3, T y3);
+
+template <typename T>
+bool intersect(T x11, T y11, T x12, T y12, T x21, T y21, T x22, T y22);
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 template <typename T>
 std::vector<T> bob_floyd(T sample_size, T upper_bound, std::mt19937_64& generator)
@@ -31,13 +42,15 @@ std::vector<T> bob_floyd(T sample_size, T upper_bound, std::mt19937_64& generato
     return samples;
 }
 
-static inline void ltrim(std::string &s) {
+static inline void ltrim(std::string &s)
+{
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
         return !std::isspace(ch);
     }));
 }
 
-static inline void rtrim(std::string &s) {
+static inline void rtrim(std::string &s)
+{
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
         return !std::isspace(ch);
     }).base(), s.end());
@@ -65,6 +78,58 @@ std::vector<std::string> split(std::string line, char c)
 void hash_combine(std::size_t& seed, const size_t v)
 {
     seed ^= v + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+bool on_segment(T x1, T y1, T x2, T y2, T x3, T y3)
+{
+    if (x1 <= std::max(x1, x3)
+            && x2 >= std::min(x1, x3)
+            && y2 <= std::max(y1, y3)
+            && y2 >= std::min(y1, y3))
+        return true;
+    return false;
+}
+
+template <typename T>
+int orientation(T x1, T y1, T x2, T y2, T x3, T y3)
+{
+    auto val = (y2 - y1) * (x3 - x2) - (x2 - x1) * (y3 - y2);
+    if (val == 0)
+        return 0;
+    return (val > 0)? 1: 2;
+}
+
+template <typename T>
+bool intersect(T x11, T y11, T x12, T y12, T x21, T y21, T x22, T y22)
+{
+    int o1 = orientation(x11, y11, x12, y12, x21, y21);
+    int o2 = orientation(x11, y11, x12, y12, x22, y22);
+    int o3 = orientation(x21, y21, x22, y22, x11, y11);
+    int o4 = orientation(x21, y21, x22, y22, x12, y12);
+
+    if (o1 != o2 && o3 != o4) {
+        if ((x11 == x21 && y11 == y21)
+                || (x11 == x22 && y11 == y22)
+                || (x12 == x21 && y12 == y21)
+                || (x12 == x22 && y12 && y22))
+            return false;
+        return true;
+    }
+
+    if (o1 == 0 && on_segment(x11, y11, x21, y21, x12, y12))
+        return true;
+    if (o2 == 0 && on_segment(x11, y11, x22, y22, x12, y12))
+        return true;
+    if (o3 == 0 && on_segment(x21, y21, x11, y11, x22, y22))
+        return true;
+    if (o4 == 0 && on_segment(x21, y21, x12, y12, x22, y22))
+        return true;
+    return false;
 }
 
 }
