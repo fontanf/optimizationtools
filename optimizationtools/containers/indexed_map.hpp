ea8ae6913a4c6b0cd45b5bdf0e48d4e055af2e2e
Florian Fontan <dev@florian-fontan.fr>
2023-09-26 07:05:10 +0200

    Update IndexedSet and IndexedMap

--- optimizationtools/containers/indexed_map.hpp
+++ optimizationtools/containers/indexed_map.hpp
@@ -2,6 +2,8 @@
 
 #include <vector>
 #include <cstdint>
+#include <random>
+#include <algorithm>
 
 namespace optimizationtools
 {
@@ -16,29 +18,81 @@ public:
     typedef int64_t Position;
     typedef typename std::vector<std::pair<Index, Value>>::const_iterator const_iterator;
 
-    inline IndexedMap(Index number_of_elements, Value null_value);
-    inline virtual ~IndexedMap() { }
+    /*
+     * Constructors and destructor
+     */
 
+    /** Constructor. */
+    inline IndexedMap(Index number_of_elements, Value null_value);
+
+    /*
+     * Getters
+     */
+
+    /** Return 'true' iff the map is empty. */
     inline bool empty() const { return number_of_elements_ == 0; }
+
+    /** Get the number of elements in the map. */
     inline Position size() const { return number_of_elements_; }
+
+    /** Retutnr 'true' iff the given element belongs to the map. */
     inline bool contains(Index index) const { return (positions_[index] < number_of_elements_); }
+
+    /** Get the value of an element. */
     inline Value operator[](Index index) const { return ((contains(index))? elements_[positions_[index]].second: null_value_); }
 
+    /** Get the begin iterator for elements inside the map. */
     inline const_iterator begin() const { return elements_.begin(); }
+
+    /** Get the end iterator for elements inside the map. */
     inline const_iterator end() const { return elements_.begin() + number_of_elements_; }
+
+    /** Get the begin iterator for elements outside the map. */
     inline const_iterator out_begin() const { return elements_.begin() + number_of_elements_; }
+
+    /** Get the end iterator for elements outside the map. */
     inline const_iterator out_end() const { return elements_.end(); }
 
+    /*
+     * Setters
+     */
+
+    /** Set the value of an element. */
     inline void set(Index index, Value value);
+
+    /** Remove all elements from the map. */
     inline void clear() { number_of_elements_ = 0; };
 
-    inline bool check() const;
+    /** Shuffle the elements of the map. */
+    inline void shuffle(std::mt19937_64& generator);
+
+    /** Shuffle the elements which belong to the map. */
+    inline void shuffle_in(std::mt19937_64& generator);
+
+    /** Shuffle a subset of elements which belong to the map. */
+    inline void shuffle_in(
+            Index number_of_elements,
+            std::mt19937_64& generator);
+
+    /** Shuffle the elements which don't belong to the map. */
+    inline void shuffle_out(std::mt19937_64& generator);
 
 private:
 
+    /*
+     * Private attributes
+     */
+
+    /** List of all elements, first inside then outside the map. */
     std::vector<std::pair<Index, Value>> elements_;
+
+    /** For each element, its position in the 'elements_' vector. */
     std::vector<Position> positions_;
+
+    /** Number of elements in the map. */
     Position number_of_elements_ = 0;
+
+    /** Value of elements which are not in the map. */
     Value null_value_;
 
 };
@@ -82,6 +136,48 @@ inline void IndexedMap<Value>::set(Index index, Value value)
             number_of_elements_++;
         }
     }
+}
+
+template <typename Value>
+inline void IndexedMap<Value>::shuffle_in(
+        Index number_of_elements,
+        std::mt19937_64& generator)
+{
+    number_of_elements = std::min(number_of_elements, number_of_elements_);
+    for (Position position = 0;
+            position < number_of_elements;
+            ++position) {
+        std::uniform_int_distribution<Position> distribution(position, number_of_elements_ - 1);
+        Position position_2 = distribution(generator);
+        std::swap(elements_[position], elements_[position_2]);
+        positions_[elements_[position]] = position;
+        positions_[elements_[position_2]] = position_2;
+    }
+}
+
+template <typename Value>
+inline void IndexedMap<Value>::shuffle_in(std::mt19937_64& generator)
+{
+    std::shuffle(elements_.begin(), elements_.begin() + number_of_elements_, generator);
+    for (Position position = 0; position < number_of_elements_; ++position)
+        positions_[elements_[position]] = position;
+}
+
+template <typename Value>
+inline void IndexedMap<Value>::shuffle_out(std::mt19937_64& generator)
+{
+    std::shuffle(elements_.begin() + number_of_elements_, elements_.end(), generator);
+    for (Position position = number_of_elements_; position < (Position)elements_.size(); ++position)
+        positions_[elements_[position]] = position;
+}
+
+template <typename Value>
+inline void IndexedMap<Value>::shuffle(std::mt19937_64& generator)
+{
+    std::shuffle(elements_.begin(), elements_.begin() + number_of_elements_, generator);
+    std::shuffle(elements_.begin() + number_of_elements_, elements_.end(), generator);
+    for (Position position = 0; position < (Position)elements_.size(); ++position)
+        positions_[elements_[position]] = position;
 }
 
 }
--- optimizationtools/containers/indexed_set.hpp
+++ optimizationtools/containers/indexed_set.hpp
@@ -33,14 +33,33 @@ public:
     typedef int64_t Index;
     typedef int64_t Position;
 
+    /*
+     * Constructors and destructor
+     */
+
+    /** Constructor. */
     inline IndexedSet(Index number_of_elements);
-    inline virtual ~IndexedSet() { }
+
+    /** Add a new possible element to the set. */
     inline void add_element();
 
+    /*
+     * Getters
+     */
+
+    /** Return 'true' iff the set is empty. */
     inline bool empty() const { return number_of_elements_ == 0; }
+
+    /** Get the number of elements in the set. */
     inline Position size() const { return number_of_elements_; }
+
+    /** Retutnr 'true' iff the given element belongs to the set. */
     inline bool contains(Index index) const { return (positions_[index] < number_of_elements_); }
 
+    /**
+     * Return 'true' iff all the elements from a given vector belong to the
+     * set.
+     */
     inline bool contains(const std::vector<Index>& indices) const
     {
         for (Index index: indices)
@@ -49,27 +68,64 @@ public:
         return true;
     }
 
+    /** Get the position of an element in the set. */
     inline Position position(Index index) const { return positions_[index]; }
 
+    /** Get the begin iterator for elements inside the set. */
     inline std::vector<Index>::const_iterator begin() const { return elements_.begin(); }
+
+    /** Get the end iterator for elements inside the set. */
     inline std::vector<Index>::const_iterator end() const { return elements_.begin() + number_of_elements_; }
+
+    /** Get the begin iterator for elements outside the set. */
     inline std::vector<Index>::const_iterator out_begin() const { return elements_.begin() + number_of_elements_; }
+
+    /** Get the end iterator for elements outside the set. */
     inline std::vector<Index>::const_iterator out_end() const { return elements_.end(); }
 
-    inline bool add(Index index);
-    inline bool remove(Index index);
-    inline void clear() { number_of_elements_ = 0; }
-    inline void fill() { number_of_elements_ = elements_.size(); }
-    inline void shuffle(std::mt19937_64& generator);
-    inline void shuffle_in(std::mt19937_64& generator);
-    inline void shuffle_out(std::mt19937_64& generator);
+    /*
+     * Setters
+     */
 
-    inline bool check() const;
+    /** Add an element to the set. */
+    inline bool add(Index index);
+
+    /** Remove an element from the set. */
+    inline bool remove(Index index);
+
+    /** Remove all elements from the set. */
+    inline void clear() { number_of_elements_ = 0; }
+
+    /** Add all elements to the set. */
+    inline void fill() { number_of_elements_ = elements_.size(); }
+
+    /** Shuffle the elements of the set. */
+    inline void shuffle(std::mt19937_64& generator);
+
+    /** Shuffle the elements which belong to the set. */
+    inline void shuffle_in(std::mt19937_64& generator);
+
+    /** Shuffle a subset of elements which belong to the set. */
+    inline void shuffle_in(
+            Index number_of_elements,
+            std::mt19937_64& generator);
+
+    /** Shuffle the elements which don't belong to the set. */
+    inline void shuffle_out(std::mt19937_64& generator);
 
 private:
 
+    /*
+     * Private attributes
+     */
+
+    /** List of all elements, first inside then outside the set. */
     std::vector<Index> elements_;
+
+    /** For each element, its position in the 'elements_' vector. */
     std::vector<Position> positions_;
+
+    /** Number of elements in the set. */
     Position number_of_elements_ = 0;
 
 };
@@ -119,6 +175,22 @@ inline bool IndexedSet::remove(Index index)
     positions_[elements_[number_of_elements_ - 1]] = number_of_elements_ - 1;
     number_of_elements_--;
     return true;
+}
+
+inline void IndexedSet::shuffle_in(
+        Index number_of_elements,
+        std::mt19937_64& generator)
+{
+    number_of_elements = std::min(number_of_elements, number_of_elements_);
+    for (Position position = 0;
+            position < number_of_elements;
+            ++position) {
+        std::uniform_int_distribution<Position> distribution(position, number_of_elements_ - 1);
+        Position position_2 = distribution(generator);
+        std::swap(elements_[position], elements_[position_2]);
+        positions_[elements_[position]] = position;
+        positions_[elements_[position_2]] = position_2;
+    }
 }
 
 inline void IndexedSet::shuffle_in(std::mt19937_64& generator)
